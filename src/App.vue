706f8f4c01d2e7ec997366c3e097fe6e9fb79eba
yuke.lu <yuke.lu@uaes.com>
2023-08-08 10:46:16 +0800

    test: 1

--- src/App.vue
+++ src/App.vue
@@ -12,13 +12,10 @@
   import { useTitle } from '@/hooks/web/useTitle';
   import { useLocale } from '@/locales/useLocale';
   import 'dayjs/locale/zh-cn';
-  import initRuntimeVar from '@/settings/runtimeVar';
   // support Multi-language
   const { getAntdLocale } = useLocale();
 
   // Listening to page changes and dynamically changing site titles
   useTitle();
 
-  //初始化运行时变量
-  initRuntimeVar();
 </script>
---
+++ src/runtimeVar.ts
@@ -0,0 +1,5 @@
+window._env_ = {
+  ENV_VARIABLE1: '(init)运行时环境变量1',
+  ENV_VARIABLE2: '(init)运行时环境变量2',
+  ENV_VARIABLE3: '(init)运行时环境变量3',
+};
