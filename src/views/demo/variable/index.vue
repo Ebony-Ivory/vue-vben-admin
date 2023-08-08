ea575fabe8e0e363e67ecb09f90851e4d31a6510
yuke.lu <yuke.lu@uaes.com>
2023-08-08 11:31:56 +0800

    feat: 添加类型声明
    添加类型声明

--- src/views/demo/variable/index.vue
+++ src/views/demo/variable/index.vue
@@ -1,7 +1,6 @@
 <template>
   <div>{{ envVar.ENV_VARIABLE1 }} </div>
   <div>{{ envVar.ENV_VARIABLE2 }} </div>
-  <div>{{ envVar.ENV_VARIABLE3 }} </div>
 </template>
 <script lang="ts" setup>
   let envVar = window._env_;
