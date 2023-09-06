005ea3c7a997fd4fc2dffbee771cdd10e125208f
yuke.lu <yuke.lu@uaes.com>
2023-09-06 09:43:23 +0800

    feat: 添加vuetify默认适配的mdi图标的cssloader
    添加vuetify默认适配的mdi图标的cssloader

--- src/components/Vuetify/index.ts
+++ src/components/Vuetify/index.ts
@@ -1,6 +1,7 @@
 import type { App } from 'vue';
 import { createVuetify } from 'vuetify';
 import 'vuetify/styles';
+import '@mdi/font/css/materialdesignicons.css';
 //引入vuetify,使用默认配置
 //不全量引入
 const vuetify = createVuetify();
--- src/views/demo2/vuetify/index.vue
+++ src/views/demo2/vuetify/index.vue
@@ -15,7 +15,7 @@
           tick-size="4"
         >
           <template v-slot:thumb-label="{ modelValue }">
-            <div class="i-mdi-snowflake"></div>
+            <VIcon theme="dark" :icon="icons[modelValue]"></VIcon>
           </template>
         </VRangeSlider>
       </VCard>
@@ -24,33 +24,44 @@
       <VCard title="进度条">
         <VDivider></VDivider>
         <div class="p-4">
-          <VProgressLinear model-value="10" color="light-blue" height="10" striped></VProgressLinear>
-        <br />
-        <VProgressLinear
-          model-value="20"
-          color="light-green-darken-4"
-          height="10"
-          striped
-        ></VProgressLinear>
-        <br />
-        <VProgressLinear model-value="45" height="10" striped color="lime"></VProgressLinear>
-        <br />
-        <VProgressLinear model-value="60" height="10" striped color="deep-orange"></VProgressLinear>
+          <VProgressLinear
+            model-value="10"
+            color="light-blue"
+            height="10"
+            striped
+          ></VProgressLinear>
+          <br />
+          <VProgressLinear
+            model-value="20"
+            color="light-green-darken-4"
+            height="10"
+            striped
+          ></VProgressLinear>
+          <br />
+          <VProgressLinear model-value="45" height="10" striped color="lime"></VProgressLinear>
+          <br />
+          <VProgressLinear
+            model-value="60"
+            height="10"
+            striped
+            color="deep-orange"
+          ></VProgressLinear>
         </div>
- 
       </VCard>
     </div>
   </div>
 </template>
 <script lang="ts" setup>
   import { ref } from 'vue';
-  import { VRangeSlider, VProgressLinear, VCard, VDivider } from 'vuetify/components';
+  import { VRangeSlider, VProgressLinear, VCard, VDivider, VIcon } from 'vuetify/components';
   let seasons = ref({
     0: 'Winter',
     1: 'Spring',
     2: 'Summer',
     3: 'Fall',
   });
+
+  let icons = ref(['mdi-snowflake', 'mdi-leaf', 'mdi-fire', 'mdi-water']);
   const loading = ref(true);
   setTimeout(() => {
     loading.value = false;
